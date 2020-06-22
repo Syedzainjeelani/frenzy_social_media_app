c68858fa14a7eb4aa6dccfd2611e679c33f76c1d
Syedzainjeelani <sayedzainjeelani@gmail.com>
2020-06-22 19:30:31 +0500

    Add FollowingCircle Widgets' horizontal List

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/data/data.dart';
+import 'package:flutter_social_ui/models/user_model.dart';
+import 'package:flutter_social_ui/widgets/following_circle.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -47,7 +50,37 @@ class _HomeScreenState extends State<HomeScreen>
           ],
         ),
       ),
-      body: Container(),
+      body: ListView(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+                child: Text(
+                  'Following',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                height: 80,
+                width: MediaQuery.of(context).size.width,
+                // width: double.infinity,
+                color: Colors.red[200],
+                child: ListView.builder(
+                    // padding: EdgeInsets.symmetric(horizontal: 5),
+                    scrollDirection: Axis.horizontal,
+                    physics: BouncingScrollPhysics(),
+                    itemCount: users.length,
+                    itemBuilder: (context, index) {
+                      User user = users[index];
+                      return FollowingCircle(user: user);
+                    }),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/following_circle.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/models/user_model.dart';
+
+class FollowingCircle extends StatelessWidget {
+  final User user;
+  FollowingCircle({this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      margin: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black38, offset: Offset(0, 2), blurRadius: 6.0),
+        ],
+        border: Border.all(width: 1, color: Colors.white54),
+      ),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(50),
+          child: Image(
+              width: 60,
+              height: 60,
+              image: AssetImage(user.profileImageUrl),
+              fit: BoxFit.cover)),
+    );
+  }
+}
